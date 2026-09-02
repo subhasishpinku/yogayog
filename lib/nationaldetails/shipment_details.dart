ef85313a1663ac669bbf09c4ce808b65a85d139e
subhasishpinku <pinku.subhasish@gmail.com>
2026-09-02 11:48:36 +0530

    today

---
+++ lib/nationaldetails/shipment_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShipmentDetails extends StatefulWidget {
+  const ShipmentDetails({super.key});
+
+  @override
+  State<ShipmentDetails> createState() => _ShipmentDetailsState();
+}
+
+class _ShipmentDetailsState extends State<ShipmentDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
